9c906e0a804cb4579beba3e1bfcda6211a289161
Kelly Hook <kellymhook@SpaceComputer.local>
2018-06-05 21:16:53 -0700

    html, css, and js added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,48 @@
-function alertRPS(event){
+//https://www.youtube.com/watch?v=tbLCMFp9QK4
 
- if (event.key == 'r') {
-  alert("Rock");
-}else if (event.key == 'p') {
-  alert("Paper");
-}else if (event.key == 's') {
-  alert("Scissors");
-}
+var randomWordArr = ['Tyrannosaurus', 'Stegosaurus', 'Velociraptor'];
+
+//choose random word from the dino array
+var randomWord = randomWordArr[Math.floor(Math.random() * randomWordArr.length)];
+
+var s;
+var count = 0;
+
+//empty array for the Guesses
+var answerArray = [];
+
+// function to match the array with the same letter hashes from randomWordArr
+
+function startUp()
+  {
+  for (var i = 0; i < randomWord.length; i++)
+  {
+    answerArray[i] = "_";
+  }
+
+  s = answerArray.join(" ");
+  document.getElementById("answer").innerHTML = s;
 }
 
-document.onkeyup = alertRPS;
+function Letter()
+{
+    //get the letter typed in the box
+    var letter = document.getElementById("letter").value;
+
+    //make sure we have a guess(can add more checks liek only letters, etc)
+    if (letter.length>0)
+    {
+      for (var i =0; i < randomWord.length; i++)
+      {
+        if (randomWord[i] === letter)
+        {
+            //assign it to letter
+          answerArray[i] = letter;
+        }
+      }
+
+      count++;
+      document.getElementbyId("counter").innerHTML = " Attempts: " + count;
+      document.getElementbyId("answer").innerHTML = answerArray.join(" ");
+    }
+  }

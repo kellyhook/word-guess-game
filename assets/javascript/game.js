05afbb46c0dc31dacbaae72893b74e543928eb0a
Kelly Hook <kellymhook@SpaceComputer.local>
2018-06-07 10:27:23 -0700

    additional dinosaur images added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 //https://www.youtube.com/watch?v=tbLCMFp9QK4
 
+startGame();
+
 var randomWordArr = ['Tyrannosaurus', 'Stegosaurus', 'Velociraptor'];
 
 //choose random word from the dino array
@@ -13,10 +15,8 @@ var answerArray = [];
 
 // function to match the array with the same letter hashes from randomWordArr
 
-function startUp()
-  {
-  for (var i = 0; i < randomWord.length; i++)
-  {
+function startGame(){
+  for (var i = 0; i < randomWord.length;i++){
     answerArray[i] = "_";
   }
 
@@ -24,16 +24,14 @@ function startUp()
   document.getElementById("answer").innerHTML = s;
 }
 
-function Letter()
-{
+function Letter(){
     //get the letter typed in the box
     var letter = document.getElementById("letter").value;
 
     //make sure we have a guess(can add more checks liek only letters, etc)
     if (letter.length>0)
     {
-      for (var i =0; i < randomWord.length; i++)
-      {
+      for (var i =0; i < randomWord.length; i++){
         if (randomWord[i] === letter)
         {
             //assign it to letter

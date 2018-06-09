61f5a59a11bcea37ca468b95d96b40e7a14990cb
Kelly Hook <kellymhook@SpaceComputer.local>
2018-06-08 22:40:26 -0700

    for loop issues fixed. Still troubleshooting random words

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,3 @@
-//https://www.youtube.com/watch?v=tbLCMFp9QK4
-
-startGame();
-
 var randomWordArr = ['Tyrannosaurus', 'Stegosaurus', 'Velociraptor'];
 
 //choose random word from the dino array
@@ -10,37 +6,38 @@ var randomWord = randomWordArr[Math.floor(Math.random() * randomWordArr.length)]
 var s;
 var count = 0;
 
-//empty array for the Guesses
 var answerArray = [];
 
-// function to match the array with the same letter hashes from randomWordArr
 
-function startGame(){
-  for (var i = 0; i < randomWord.length;i++){
+function startGame() {
+  console.log("here");
+  // Set up the answer array
+  for (var i = 0; i < randomWord.length; i++) {
     answerArray[i] = "_";
   }
 
   s = answerArray.join(" ");
+  console.log(s);
   document.getElementById("answer").innerHTML = s;
 }
 
-function Letter(){
-    //get the letter typed in the box
-    var letter = document.getElementById("letter").value;
+function Letter() {
+  //get the letter typed in the box
+  var letter = document.getElementById("letter").value;
 
-    //make sure we have a guess(can add more checks liek only letters, etc)
-    if (letter.length>0)
-    {
-      for (var i =0; i < randomWord.length; i++){
-        if (randomWord[i] === letter)
-        {
-            //assign it to letter
-          answerArray[i] = letter;
-        }
+  //make sure we have a guess(can add more checks liek only letters, etc)
+  if (letter.length > 0) {
+    for (var i = 0; i < randomWord.length; i++) {
+      if (randomWord[i] === letter) {
+        //assign it to letter
+        answerArray[i] = letter;
       }
-
-      count++;
-      document.getElementbyId("counter").innerHTML = " Attempts: " + count;
-      document.getElementbyId("answer").innerHTML = answerArray.join(" ");
     }
+
+    count++;
+    document.getElementById("counter").innerHTML = "No. of clicks " + count;
+    document.getElementById("answer").innerHTML = answerArray.join(" ");
   }
+}
+console.log("there");
+startGame();
